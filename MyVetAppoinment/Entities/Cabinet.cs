18ae8e57fd684f0a7294f6a95c8f5519b17eaa62
RusuMagda <100140417+RusuMagda@users.noreply.github.com>
2022-11-09 14:37:53 +0200

    Update Cabinet.cs

--- MyVetAppoinment/Entities/Cabinet.cs
+++ MyVetAppoinment/Entities/Cabinet.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyVetAppoinment.Entities
 {
     public class Cabinet
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Client> Clients { get; set; }
+        public Shop Shop { get; set; }
+
+
     }
 }
