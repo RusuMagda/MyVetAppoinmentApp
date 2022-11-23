832c36c1f3177829d66c0821dddc47c79eef48da
Roca Andreea <andreearoca7@yahoo.com>
2022-11-23 16:19:52 +0200

    Update Appointment.cs

--- MyVetAppointment.Domain/Entities/Appointment.cs
+++ MyVetAppointment.Domain/Entities/Appointment.cs
@@ -2,34 +2,36 @@
 {
     public class Appointment
     {
-        public Appointment(DateTime startTime, DateTime endTime, string description)
+        public Appointment(DateTime startTime, DateTime endTime, string description, Guid petId, Guid cabinetId)
         {
             Id = Guid.NewGuid();
             StartTime = startTime;
             EndTime = endTime;
             Description = description;
+            PetId = petId;
+            CabinetId = cabinetId;
         }
 
         public Guid Id { get; private set; } 
 
-        public Pet Pet { get; private set; }
+        public Guid PetId { get; private set; }
 
         public DateTime StartTime { get; private set; }
         public DateTime EndTime { get; private set; }
 
         public string Description { get; private set; }
 
-        public Cabinet Cabinet { get; private set; }
-
+        public Guid CabinetId { get; private set; }
+/*
         public void attachPet(Pet pet)
         {
-            Pet = pet;
+            PetId = pet.Id;
         }
 
         public void attachCabinet(Cabinet cabinet)
         {
-            Cabinet = cabinet;
-        }
+            CabinetId = cabinet.Id;
+        }*/
 
     }
 }
