3dac69e8c29d5d4838ad70d23f6bf6cc66fb07ed
Roca Andreea <andreearoca7@yahoo.com>
2022-11-23 15:22:34 +0200

    AppointmentConstroller

---
+++ MyVetAppointment.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyVetAppoinment.Domain.Entities;
+using MyVetAppoinment.Repositories;
+using MyVetAppointment.API.DTOs;
+
+namespace MyVetAppointment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IAppointmentRepository appointmentRepository;
+
+        public AppointmentsController(IAppointmentRepository appointmentRepository)
+        {
+            this.appointmentRepository = appointmentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(appointmentRepository.GetAll());
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult Get(Guid id)
+        {
+            var appointment = appointmentRepository.Get(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return Ok(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateAppointmentDto dto)
+        {
+            var appointment = new Appointment(dto.StartTime, dto.EndTime, dto.Description);
+            appointmentRepository.Add(appointment);
+            appointmentRepository.Save();
+            return Created(nameof(Get), appointment);
+        }
+    }
+}
---
+++ MyVetAppointment.API/DTOs/CreateAppointmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVetAppointment.API.DTOs
+{
+    public class CreateAppointmentDto
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+    }
+}
