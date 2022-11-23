633a2c6ec58f9e18d738a8dff6b630e0ad66ae2d
Magda <rusumagda3002@gmail.com>
2022-11-23 13:41:01 +0200

    Cabinet Contoller

---
+++ MyVetAppointment.API/Controllers/CabinetesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyVetAppoinment.Domain.Entities;
+using MyVetAppoinment.Repositories;
+using MyVetAppointment.API.DTOs;
+
+namespace MyVetAppointment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CabinetesController : ControllerBase
+    {
+        private readonly ICabinetRepository cabinetRepository;
+
+        public CabinetesController(ICabinetRepository cabinetRepository)
+        {
+            this.cabinetRepository = cabinetRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(cabinetRepository.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateCabinetDto dto)
+        {
+            var cabinet = new Cabinet(dto.Name, dto.Address);
+            cabinetRepository.Add(cabinet);
+            cabinetRepository.Save();
+            return Created(nameof(Get), cabinet);
+
+        }
+    }
+}
