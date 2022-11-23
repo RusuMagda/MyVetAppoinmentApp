a5679850e9e689fecee5170b8167426c883f4f15
Ciobanu Ana <ciobanu.nica@bk.ru>
2022-11-23 14:28:37 +0200

    drugDTO

---
+++ MyVetAppointment.API/DTOs/CreateDrugDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MyVetAppointment.API.DTOs
+{
+    public class CreateDrugDto
+    {
+        public string DrugName { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public string SaleForm { get; set; }
+        public int Quantity { get; set; }
+        public string QuantityMeasure { get; set; }
+    }
+}
