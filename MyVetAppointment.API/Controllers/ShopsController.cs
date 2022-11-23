d1dc6a5d0f34e1f7a03cc7989e8be50d21ad1267
erikaonofrei <79272088+erikaonofrei@users.noreply.github.com>
2022-11-23 17:00:30 +0200

    Shop Controller

---
+++ MyVetAppointment.API/Controllers/ShopsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyVetAppoinment.Domain.Entities;
+using MyVetAppoinment.Repositories;
+using MyVetAppointment.API.DTOs;
+
+namespace MyVetAppointment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+        private readonly IShopRepository shopRepository;
+
+        public ShopsController(IShopRepository shopRepository)
+        {
+            this.shopRepository = shopRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(shopRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var shop = shopRepository.Get(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            return Ok(shop);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateShopDto dto)
+        {
+            var shop = new Shop(dto.ShopName);
+            shopRepository.Add(shop);
+            shopRepository.Save();
+            return Created(nameof(Get), shop);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(Guid id)
+        {
+            var shop = shopRepository.Get(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            shopRepository.Delete(shop);
+            shopRepository.Save();
+            return NoContent();
+        }
+    }
+}
