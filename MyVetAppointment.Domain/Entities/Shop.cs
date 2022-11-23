53220b36a08e7c48639a888abd6846a81cdb231e
Magda <rusumagda3002@gmail.com>
2022-11-23 16:03:23 +0200

    Migrare noua

--- MyVetAppointment.Domain/Entities/Shop.cs
+++ MyVetAppointment.Domain/Entities/Shop.cs
@@ -9,9 +9,15 @@
         }
 
         public Guid ShopId { get; private set; }
-        public string ShopName { get; private set; }    
+        public string ShopName { get; private set; }  
+        public Guid CabinetId { get; private set; }
         public List<Drug> Drugs { get; private set; }
 
+        public void AttachShopToCabinet(Cabinet cabinet)
+        {
+            CabinetId = cabinet.Id;
+        }
+       
         public void addDrug(Drug drug)
         {
             Drugs.Add(drug);
---
+++ MyVetAppointment.Infrastructure/Migrations/20221123131814_Second.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyVetAppointment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cabinets_Shops_ShopId",
+                table: "Cabinets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cabinets_ShopId",
+                table: "Cabinets");
+
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Cabinets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CabinetId",
+                table: "Shops",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shops_CabinetId",
+                table: "Shops",
+                column: "CabinetId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shops_Cabinets_CabinetId",
+                table: "Shops",
+                column: "CabinetId",
+                principalTable: "Cabinets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shops_Cabinets_CabinetId",
+                table: "Shops");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shops_CabinetId",
+                table: "Shops");
+
+            migrationBuilder.DropColumn(
+                name: "CabinetId",
+                table: "Shops");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShopId",
+                table: "Cabinets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cabinets_ShopId",
+                table: "Cabinets",
+                column: "ShopId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cabinets_Shops_ShopId",
+                table: "Cabinets",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "ShopId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
