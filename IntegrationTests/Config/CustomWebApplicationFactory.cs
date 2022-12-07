928880740f8f039cda3bc1da9cab2bb63f08d2cd
erikaonofrei <79272088+erikaonofrei@users.noreply.github.com>
2022-12-07 16:38:00 +0200

    refactoring

--- IntegrationTests/Config/CustomWebApplicationFactory.cs
+++ IntegrationTests/Config/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -44,6 +45,7 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
                 SeedDrugs(context);
                 SeedPets(context);
                 SeedAppointments(context);
+                SeedShops(context);
             }
             catch (Exception)
             {
