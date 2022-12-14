0954dcd3748a05593c9bc8070345e5bbf2e1a795
Ciobanu Ana <ciobanu.nica@bk.ru>
2022-12-14 13:37:04 +0200

    Phone Valid

--- MyVetAppointment.API/Validators/ClientValidator.cs
+++ MyVetAppointment.API/Validators/ClientValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using MyVetAppoinment.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.RegularExpressions;
 
 namespace MyVetAppointment.API.Validators
 {
@@ -9,7 +11,12 @@ namespace MyVetAppointment.API.Validators
         {
             RuleFor(client => client.Name).NotNull();
             RuleFor(client => client.EMail).NotNull().EmailAddress();
-            RuleFor(client => client.PhoneNumber).NotNull().MaximumLength(50);
+            RuleFor(client => client.PhoneNumber).NotNull().MaximumLength(10).Must(IsPhoneValid);
+        }
+        public static bool IsPhoneValid(string phoneNumber)
+        {
+            string regex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+                return Regex.IsMatch(phoneNumber, regex);
         }
     }
 }
