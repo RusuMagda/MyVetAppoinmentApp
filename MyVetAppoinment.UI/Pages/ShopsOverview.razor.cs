76b457aee40bd701c0fd96ef4b127f9204b30378
Oana024 <mocanuoana52@yahoo.com>
2023-01-03 23:14:17 +0200

    view cabinet name

--- MyVetAppoinment.UI/Pages/ShopsOverview.razor.cs
+++ MyVetAppoinment.UI/Pages/ShopsOverview.razor.cs
@@ -12,27 +12,21 @@ namespace MyVetAppoinment.UI.Pages
         public ICabinetDataService CabinetDataService { get; set; } = default!;
 
         public List<Shop> Shops { get; set; } = default!;
-        protected List<Cabinet> userList = default!;
-        protected Cabinet cabinet = default!;
-        protected String cabinetName = string.Empty;
-       
+        public List<Cabinet> Cabinets { get; set; } = default!;
+
         protected async override Task OnInitializedAsync()
         {
             var result = await ShopDataService.GetAllShops();
+            var CabinetsList = await CabinetDataService.GetAllCabinets();
             if (result != null)
             {
                 Shops = result.ToList();
             }
-           
-            foreach (var shop in Shops)
+
+            if (CabinetsList != null)
             {
-                var MyShop = await CabinetDataService.GetCabinetDetail(shop.CabinetId);
-                if (MyShop != null)
-                {
-                    cabinet = MyShop;
-                }
+                Cabinets = CabinetsList.ToList();
             }
-           
         }
 
     }
